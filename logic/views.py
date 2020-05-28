aa79e0a07755bcbd6951c840c3217f0900d13369
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-05-28 23:28:00 +0800

    resets redirect url

--- logic/views.py
+++ logic/views.py
@@ -22,11 +22,11 @@ def f(request):
     2. get devices for device id
     3. done'''
     #user, id, secret = getData(reqeust.get_full_path())
-    user = "Iz Ho"
+    user = "x
     id = "e28a9982e181499dbdb8b8ae80e1e993"
     secret = "3cb162e2569e423aa3b1dde6493c9e14"
     scopes = "user-read-playback-state user-modify-playback-state"
-    token = spotipy.util.prompt_for_user_token(user, scopes, client_id=id, client_secret=secret, redirect_uri="https://spotify-rest.herokuapp.com/")
+    token = spotipy.util.prompt_for_user_token(user, scopes, client_id=id, client_secret=secret, redirect_uri="http://localhost:8888")
     sp = spotipy.Spotify(auth=token)
     deviceId = sp.devices()['devices'][0]['id']
     sp.start_playback(device_id=deviceId)
