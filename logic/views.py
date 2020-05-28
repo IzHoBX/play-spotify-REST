9da9f2b7c69fe7b07039661985c0dcece3cb1a63
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-05-28 18:46:33 +0800

    uses fed token

--- logic/views.py
+++ logic/views.py
@@ -24,11 +24,11 @@ def f(request):
     #user, id, secret = getData(reqeust.get_full_path())
     print("received")
     user = "Iz Ho"
-    id = "e28a9982e181499dbdb8b8ae80e1e993"
+    '''id = "e28a9982e181499dbdb8b8ae80e1e993"
     secret = "3cb162e2569e423aa3b1dde6493c9e14"
     scopes = "user-read-playback-state user-modify-playback-state"
-    token = spotipy.util.prompt_for_user_token(user, scopes, client_id=id, client_secret=secret, redirect_uri="http://localhost:8888")
-    print("token done")
+    token = spotipy.util.prompt_for_user_token(user, scopes, client_id=id, client_secret=secret, redirect_uri="http://localhost:8888")'''
+    token = "BQAHh2r6szVXwPEVCHSrX9C00qBYlEoBJUTMXMZX96cWvLcseDRTcxjRwIDlCdZvvATb0i58s1EAsKxODDgunh32Pk1OS2NdVYNOP9BrLZj7AzSllPzI2e0GEA4CVLc_RYe55gyMg7LZeMpewjL_MHX8eB60"
     sp = spotipy.Spotify(auth=token)
     deviceId = sp.devices()['devices'][0]['id']
     sp.start_playback(device_id=deviceId)
